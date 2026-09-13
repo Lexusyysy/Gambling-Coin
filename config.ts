bddd72da84631d9ffec8d1eb89f294498fc4cfea
Lexusyysy <132613407+Lexusyysy@users.noreply.github.com>
2026-09-13 19:20:41 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ export const telegramWebApp = `https://pekkycoinbot.t.me?startapp&utm_source=${m
 
 // Socials
 export const instagramLink = `https://www.instagram.com/pekkytoken?utm_source=${mainLink}`; // Instagram
-export const telegramLink = `https://t.me/pekkycoin?utm_source=${mainLink}`; // Telegram
+export const telegramLink = `https://t.me/pekkysol?utm_source=${mainLink}`; // Telegram
 export const twitterLink = `https://x.com/pekky_sol?utm_source=${mainLink}`; // Twitter
 
 // Token Info
